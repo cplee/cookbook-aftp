5e01af0c49739707319109b40fb09b24d14f0125
Casey Lee <casele@vsp.com>
2015-11-03 16:42:37 -0800

    serverspec works

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,3 +1,19 @@
+require 'serverspec'
+
 describe package('httpd')do
   it { should be_installed }
+end
+
+describe service('httpd-default') do
+  it {should be_enabled}
+  it {should be_running}
+end
+
+describe port(80) do
+  it {should be_listening}
+end
+
+describe command('curl http://localhost') do
+  its(:exit_status) { should eq 0 }
+  its(:stdout) { should match /Automation for the People/ }
 end
